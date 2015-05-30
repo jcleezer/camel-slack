e54eaeccf7e2c6e6fde1c93fec3fb3e258e7572a
Mike Kennedy <michael.kennedy@ucr.edu>
2015-05-30 01:34:18 -0700

    Fill in a SlackMessage object with relevent slack message attrs

--- src/main/java/io/mikekennedy/camel/SlackProducer.java
+++ src/main/java/io/mikekennedy/camel/SlackProducer.java
@@ -2,6 +2,8 @@ package io.mikekennedy.camel;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultProducer;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +19,29 @@ public class SlackProducer extends DefaultProducer {
     public SlackProducer(SlackEndpoint endpoint) {
         super(endpoint);
         this.slackEndpoint = endpoint;
+        LOG.info("webHookUrl = " + this.slackEndpoint.getWebhookUrl());
     }
 
     @Override
     public void process(Exchange exchange) throws Exception {
         HttpClient client = HttpClientBuilder.create().build();
 
+        HttpPost httpPost = new HttpPost(slackEndpoint.getWebhookUrl());
+
+        SlackMessage slackMessage = new SlackMessage();
+
+        slackMessage.setText((String)exchange.getIn().getBody());
+        slackMessage.setChannel(slackEndpoint.getChannel());
+        slackMessage.setUsername(slackEndpoint.getUsername());
+        slackMessage.setIconUrl(slackEndpoint.getIconUrl());
+        slackMessage.setIconEmoji(slackEndpoint.getIconEmoji());
+
+        StringEntity body = new StringEntity(slackMessage.toString());
+
+        LOG.info(slackMessage.toString());
+
+        httpPost.setEntity(body);
+
+        client.execute(httpPost);
     }
 }
