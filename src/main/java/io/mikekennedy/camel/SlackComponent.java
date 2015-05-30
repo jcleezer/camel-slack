9f694db30f30c57d007febdce616a1f1c6452351
Mike Kennedy <michael.kennedy@ucr.edu>
2015-05-30 01:32:24 -0700

    Get channel name from the remaining (after component) value

--- src/main/java/io/mikekennedy/camel/SlackComponent.java
+++ src/main/java/io/mikekennedy/camel/SlackComponent.java
@@ -12,14 +12,10 @@ public class SlackComponent extends DefaultComponent {
     private static final transient Logger LOG = LoggerFactory.getLogger(SlackComponent.class);
 
     private String webhookUrl;
-    private String username;
-    private String channel;
-    private String iconUrl;
-    private String iconEmoji;
 
     @Override
-    protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
-        Endpoint endpoint = new SlackEndpoint(uri, this);
+    protected Endpoint createEndpoint(String uri, String channelName, Map<String, Object> parameters) throws Exception {
+        Endpoint endpoint = new SlackEndpoint(uri, channelName, this);
         setProperties(endpoint, parameters);
         return endpoint;
     }
@@ -30,37 +26,5 @@ public class SlackComponent extends DefaultComponent {
 
     public void setWebhookUrl(String webhookUrl) {
         this.webhookUrl = webhookUrl;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getChannel() {
-        return channel;
-    }
-
-    public void setChannel(String channel) {
-        this.channel = channel;
-    }
-
-    public String getIconUrl() {
-        return iconUrl;
-    }
-
-    public void setIconUrl(String iconUrl) {
-        this.iconUrl = iconUrl;
-    }
-
-    public String getIconEmoji() {
-        return iconEmoji;
-    }
-
-    public void setIconEmoji(String iconEmoji) {
-        this.iconEmoji = iconEmoji;
     }
 }
