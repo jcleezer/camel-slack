6e768ef90ecfc9c3bd2726b3485cc67ac6d2d117
Mike Kennedy <michael.kennedy@ucr.edu>
2015-05-30 18:25:40 -0700

    javadoc added

--- src/main/java/io/mikekennedy/camel/SlackComponent.java
+++ src/main/java/io/mikekennedy/camel/SlackComponent.java
@@ -13,6 +13,15 @@ public class SlackComponent extends DefaultComponent {
 
     private String webhookUrl;
 
+    /**
+     * Create a slack endpoint
+     *
+     * @param uri the full URI of the endpoint
+     * @param channelName the channel or username that the message should be sent to
+     * @param parameters the optional parameters passed in
+     * @return the camel endpoint
+     * @throws Exception
+     */
     @Override
     protected Endpoint createEndpoint(String uri, String channelName, Map<String, Object> parameters) throws Exception {
         Endpoint endpoint = new SlackEndpoint(uri, channelName, this);
@@ -20,10 +29,20 @@ public class SlackComponent extends DefaultComponent {
         return endpoint;
     }
 
+    /**
+     * Getter for the incoming webhook URL
+     *
+     * @return String containing the incoming webhook URL
+     */
     public String getWebhookUrl() {
         return webhookUrl;
     }
 
+    /**
+     * Setter for the incoming webhook URL
+     *
+     * @param webhookUrl the incoming webhook URL
+     */
     public void setWebhookUrl(String webhookUrl) {
         this.webhookUrl = webhookUrl;
     }
